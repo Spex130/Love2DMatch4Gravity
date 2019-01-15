6420265827745bdb3ed1261f0d114cd94ce7ba1a
Larry M. Smith <lsmith0093@gmail.com>
2019-01-15 11:29:29 -0500

    Finished an actual tutorial, time for a hard refresh. (Deleted Stage as well)

--- block.lua
+++
@@ -1,7 +0,0 @@
-Block = Object:extend()
-
-function Block:new(x, y, color)
-	self.x = x or 0
-	self.y = y or 0
-	self.color = 1
-end
--- main.lua
+++ main.lua
@@ -27,86 +27,22 @@ canDropTimer = canDropTimerMax
 --Gamestate tracking
 gameState = 1
 
---Controllable falling attributes
-fallingPair = {}
 
-
-	function love.load(arg)
-		importClasses()
-		
-		
-	--Code start
-		love.graphics.setBackgroundColor(255, 255, 255)
-		
-		loadBlocks()
-		
-		
-		
-
-		inert = {}
-		for y = 1, gridYCount do
-			inert[y] = {}
-			for x = 1, gridXCount do
-				inert[y][x] = colorBlank
-			end
-		end
-	end
-
+function love.load(arg)
+	
+end
 	function love.update(dt)
-		updateGameState()
-		dropTimerUpdate(dt)
+
 	end
 
 	function love.draw(dt)
-		drawInertBlocks()
+
 	end
 
 	function loadBlocks()
 
 		blocksPGBY = {love.graphics.newImage('assets/blocksPur.png'), love.graphics.newImage('assets/blocksGre.png'), love.graphics.newImage('assets/blocksBlu.png'), love.graphics.newImage('assets/blocksYel.png')}
 
-	end
-
-	function drawGrid()
-		for y = 1, gridYCount do
-			for x = 1, gridXCount do
-				love.graphics.setColor(.87, .87, .87)
-				local blockSize = 20
-				local blockDrawSize = blockSize - 1
-				love.graphics.rectangle(
-					'fill',
-					(x - 1) * blockSize,
-					(y - 1) * blockSize,
-					blockDrawSize,
-					blockDrawSize
-				)
-			end
-		end
-	end
-
-	function drawGridBlock(color, col, row)
-		if(color ~= colorBlank) then
-			love.graphics.draw(blocksPGBY[1], gridOrigin[1] + gridBlockWidth* (col-1) , gridOrigin[2] + gridBlockWidth * (row-1))
-		end
-	end
-
-	function drawInertBlocks()
-		for x = 1, gridXCount do
-
-			for y = 1, gridYCount do
-				--drawGridBlock(inert[y][x], x, y)
-				color = inert[y][x]
-				if(color ~= colorBlank) then
-					love.graphics.draw(blocksPGBY[color], gridOrigin[1] + gridBlockWidth* (x-1) , gridOrigin[2] + gridBlockWidth*(y-1), 0, .5)
-				end
-			end
-		end
-	end
-	
-	function handleFall()
-		--Do some stuff
-		canDrop = false
-		canDropTimer = canDropTimerMax
 	end
 	
 
@@ -146,9 +82,4 @@ fallingPair = {}
 		if canDropTimer < 0 then
 		  canDrop = true
 		end
-	end
-
-	function importClasses()
-		Object = require "classic"
-		require "block"
 	end
