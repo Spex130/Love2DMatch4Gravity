94611d0d50d513e0906a9377805f8069e8e38fbe
Larry M. Smith <lsmith0093@gmail.com>
2019-02-26 11:11:01 -0500

    Additions to BlockClasses (WIP)

--- classes/blockclasses.lua
+++ classes/blockclasses.lua
@@ -3,9 +3,10 @@ GlobalVarObject = Object:extend()
 
 gameStates = {MainMenu = 1, SinglePlayer = 2, GameOver = 3}
 
-function GlobalVarObject:new(screenX, screenY, gamestate, blockSize)
+function GlobalVarObject:new(screenX, screenY, gamestate, blockSize, players)
 	self.screenX = screenX or 800
 	self.screenY = screenY or 1000
 	self.gamestate = gamestate or gameStates.MainMenu
 	self.blockSize = blockSize or 30
+	self.players = players or {}
 end
