fc3fd32f71f4ad75cb5416b0051f8a4078c3e7c8
Spex130 <Gamerguy56@bellsouth.net>
2019-08-27 16:24:29 -0400

    Found a SICK.lua that's not a dropbox 404 HTML file.

--- sick.lua
+++ sick.lua
@@ -1,18 +1,64 @@
-<!DOCTYPE html>
-<html>
-<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Dropbox - 404</title>
-<link href="https://cfl.dropboxstatic.com/static/css/error.css" rel="stylesheet" type="text/css"/>
-<link rel="shortcut icon" href="https://cfl.dropboxstatic.com/static/images/favicon.ico"/>
+-- SICK: Simple Indicative of Competitive sKill
+-- Version 2.0 - because the old one sucked -- Nat
 
-</head>
-<body>
-<div class="figure">
-<img src="https://cfl.dropboxstatic.com/static/images/illustration_catalog/404_error-illo.png" srcset="https://cfl.dropboxstatic.com/static/images/illustration_catalog/404_error-illo@2x.png 2x" alt="Error: 404"/>
-</div>
-<div id="errorbox">
-<div class="not-found"> <h1>Error (404)</h1> We can't find the page you're looking for. <div class="not-found--links"> Here are a few links that may be helpful: <ul> <li><a href="https://www.dropbox.com/home?_tk=fof">Home</a></li> <li><a href="https://www.dropbox.com/help?_tk=fof">Help center</a></li> <li><a href="https://www.dropbox.com/login?_tk=fof">Sign in</a></li> <li><a href="https://www.dropbox.com/register?_tk=fof">Get a free account</a></li> <li><a href="https://www.dropbox.com/plus?_tk=fof">Dropbox Plus</a></li> <li><a href="https://www.dropbox.com/business?_tk=fof">Dropbox Business</a></li> </ul> </div> </div>
-</div>
+local h = {}
+h.scores = {}
 
-</body>
-</html>
+function h.set(filename, places, name, score)
+   h.filename = filename
+   h.places = places
+
+   if not h.load() then
+      h.scores = {}
+      for i = 1, places, 1 do
+         h.scores[i] = {score, name}
+      end
+   end
+end
+
+function h.load()
+   if not love.filesystem.exists(h.filename) then return end
+
+   local file = love.filesystem.newFile(h.filename)
+   if not file:open("r") then return end
+
+   h.scores = {}
+   for line in file:lines() do
+      local i = line:find('\t', 1, true)
+      h.scores[#h.scores+1] = {tonumber(line:sub(1, i-1)), line:sub(i+1)}
+   end
+   return file:close()
+end
+
+local function sortScore(a, b)
+   return a[1] > b[1]
+end
+
+function h.add(name, score)
+   h.scores[#h.scores+1] = {score, name}
+   table.sort(h.scores, sortScore)
+end
+
+function h.save()
+   local data = ""
+   for i = 1, #h.scores do
+      item = h.scores[i]
+      data = data .. item[1] .. "\t" .. item[2] .. "\n"
+   end
+
+   return love.filesystem.write(h.filename, data)
+end
+
+setmetatable(h, {__call = function(self)
+   local i = 0
+   return function()
+      i = i + 1
+      if i <= h.places and h.scores[i] then
+         return i, unpack(h.scores[i])
+      end
+   end
+end})
+
+highscore = h
+
+return h
